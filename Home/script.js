b8bfed8ea5ab264ac9a77e08024d75119a24b6a6
Christian Garcia <cagarcia2@student.apc.edu.ph>
2025-01-14 16:54:17 +0000

    deleted old

--- Home/script.js
+++
@@ -1,8 +0,0 @@
-function toggleSection(sectionId) {
-    const section = document.getElementById(sectionId);
-    if (section.classList.contains('active')) {
-        section.classList.remove('active');
-    } else {
-        section.classList.add('active');
-    }
-}
